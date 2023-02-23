c4fad862b1f0ef6921ad3817c040a7b0dfd86d61
adiliqbal02 <118364388+adiliqbal02@users.noreply.github.com>
2023-02-23 10:04:54 +0000

    GFORMS-2067: Upgrade gform-stubs repository to latest scala version 2.13.8 (#19)
    * GFORMS-2049: modified appDepencies to enusre they work with the updated scala version (2.13.8)
    
    * GFORMS-2049: updated minor changes from feedback
    
    * GFORMS-2049: updated minor changes from feedback
    
    * GFORMS-2049: updated minor changes from feedback

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,17 +4,18 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28" % "5.12.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28" % "7.13.0",
     "org.scalacheck"          %% "scalacheck"                % "1.15.4"
   )
 
   val test = Seq(
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play"        % "5.1.0"                 % "test, it",
     "org.scalatestplus"       %% "scalacheck-1-15"          % "3.2.10.0"              % "test, it",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "uk.gov.hmrc"             %% "service-integration-test" % "1.2.0-play-28"         % "test, it",
-    "com.eed3si9n"            %% "gigahorse-okhttp"         % "0.3.1"                 % "test, it",
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % "test, it"
+    "uk.gov.hmrc"             %% "service-integration-test" % "1.3.0-play-28"         % "test, it",
+    "com.eed3si9n"            %% "gigahorse-okhttp"         % "0.7.0"                 % "test, it",
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "7.13.0"                % "test, it"
   )
 
 }
