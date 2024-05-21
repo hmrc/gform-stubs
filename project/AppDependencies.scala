95045128daadc048299939068e49a0308c64fbb9
Alex Olkhovskiy <lxol@users.noreply.github.com>
2024-05-21 10:43:45 +0100

    GFORMS-2501: Upgrade gform-stubs repository to play 3.0 (#34)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,17 +3,18 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "8.6.0"
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28" % "7.13.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30" % bootstrapVersion,
   )
 
   val test = Seq(
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % "test, it",
-    "org.scalatestplus"       %% "scalacheck-1-15"          % "3.2.10.0"              % "test, it",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "com.eed3si9n"            %% "gigahorse-okhttp"         % "0.7.0"                 % "test, it",
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % "test, it",
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "7.13.0"                % "test, it"
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % "test",
+    "org.scalatestplus"       %% "scalacheck-1-15"          % "3.2.10.0"              % "test",
+    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test",
+    "com.eed3si9n"            %% "gigahorse-okhttp"         % "0.7.0"                 % "test",
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % "test",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootstrapVersion        % "test"
   )
 
 }
