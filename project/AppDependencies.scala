599ac626c5dfa5f90d3e9b7c61999a5dc9a98389
Josh Martin <josh.martin@digital.hmrc.gov.uk>
2025-08-29 15:46:19 +0100

    GFORMS-3564 - PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "8.6.0"
+  val bootstrapVersion = "10.1.0"
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30" % bootstrapVersion,
   )
@@ -11,7 +11,6 @@ object AppDependencies {
   val test = Seq(
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % "test",
     "org.scalatestplus"       %% "scalacheck-1-15"          % "3.2.10.0"              % "test",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test",
     "com.eed3si9n"            %% "gigahorse-okhttp"         % "0.7.0"                 % "test",
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % "test",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootstrapVersion        % "test"
