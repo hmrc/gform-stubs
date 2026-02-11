3de9c380af5240ee22894b2b822af4c158e220d7
Pavel Vjalicin <pavelvjalicin@users.noreply.github.com>
2026-02-11 14:12:45 +0000

    GFORMS-3820 - Caseflow API integration – Case Reference ID check

---
+++ app/uk/gov/hmrc/gformstubs/controllers/CaseflowGetDetails.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.gformstubs.controllers
+
+import play.api.mvc.{ AbstractController, Action, AnyContent, ControllerComponents }
+
+import javax.inject.Inject
+
+class CaseflowGetDetails @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
+
+  //https://admin.tax.service.gov.uk/integration-hub/apis/details/61cffebd-1f98-4424-b048-f974e77a64f1
+  def getCaseDetails(caseId: String): Action[AnyContent] = Action { _ =>
+    val response = caseId.toUpperCase match {
+      case "CFS-10404" => Ok("""{
+                               |  "caseStatus": "Open",
+                               |  "oudnStatus": "oudn Exists",
+                               |  "resolvedTimestamp": "20260117T201550.325 GMT",
+                               |  "pyHTTPResponseCode": 1
+                               |}""".stripMargin) //"pyHTTPResponseCode": 1 - means that case id does not exist
+      case "CFS-10200" => Ok("""{
+                               |  "caseStatus": "Open",
+                               |  "oudnStatus": "oudn Exists",
+                               |  "resolvedTimestamp": "20260117T201550.325 GMT",
+                               |  "pyHTTPResponseCode": 200
+                               |}""".stripMargin)
+      case "CFS-10400" => BadRequest("""{
+                                       |  "origin": "HIP",
+                                       |  "response": [
+                                       |    {
+                                       |      "type": "Type of Failure",
+                                       |      "reason": "Reason for Failure"
+                                       |    }
+                                       |  ]
+                                       |}""".stripMargin)
+      case "CFS-10500" => InternalServerError("""{
+                                                |  "origin": "HIP",
+                                                |  "response": [
+                                                |    {
+                                                |      "type": "Type of Failure",
+                                                |      "reason": "Reason for Failure"
+                                                |    }
+                                                |  ]
+                                                |}""".stripMargin)
+      case "CFS-10503" => ServiceUnavailable("""{
+                                               |  "origin": "HIP",
+                                               |  "response": [
+                                               |    {
+                                               |      "type": "Type of Failure",
+                                               |      "reason": "Reason for Failure"
+                                               |    }
+                                               |  ]
+                                               |}""".stripMargin)
+      case _           => Ok("""{
+                     |  "caseStatus": "Open",
+                     |  "oudnStatus": "oudn Exists",
+                     |  "resolvedTimestamp": "20260117T201550.325 GMT",
+                     |  "pyHTTPResponseCode": 200
+                     |}""".stripMargin)
+    }
+
+    response.as("application/json")
+  }
+}
