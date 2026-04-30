c8af6df0f9ad01c1371b21b6d7bf1a650b87653b
Carl Manson <506508+cmanson@users.noreply.github.com>
2026-04-30 09:22:57 +0100

    GFORMS-3822 - Add stub for India FTA registration (#68)

--- app/uk/gov/hmrc/gformstubs/controllers/ContainerManagedArchitecture.scala
+++ app/uk/gov/hmrc/gformstubs/controllers/ContainerManagedArchitecture.scala
@@ -16,7 +16,9 @@
 
 package uk.gov.hmrc.gformstubs.controllers
 
+import play.api.libs.json.{ Json, OFormat }
 import play.api.mvc.{ AbstractController, ControllerComponents }
+import uk.gov.hmrc.gformstubs.model.IndiaFTARegistration
 
 import java.time.temporal.ChronoUnit
 import java.time.{ Instant, ZoneOffset, ZonedDateTime }
@@ -43,4 +45,33 @@ class ContainerManagedArchitecture @Inject() (controllerComponents: ControllerCo
       ).as("application/json")
     )
   }
+
+  def indiaFTARegister() = Action(parse.json[IndiaFTARegistration]) { request =>
+    val businessName = request.body.BusinessName.toLowerCase()
+    val correlationId = request.headers.get("X-Correlation-Id")
+    val instant: String =
+      ZonedDateTime.ofInstant(Instant.now(), ZoneOffset.UTC).truncatedTo(ChronoUnit.SECONDS).toString
+
+    if (businessName.contains("error"))
+      InternalServerError(
+        s"""|{
+            |  "errorDetail": {
+            |    "errorCode": "500",
+            |    "errorMessage": "Unknown error occurred",
+            |    "source": "journey-<journey-name>-service-camel",
+            |    "sourceFaultDetail": {},
+            |    "timestamp": "$instant",
+            |    "correlationId": "${correlationId.getOrElse("unknown")}"
+            |  }
+            |}""".stripMargin
+      ).as("application/json")
+    else
+      Ok(
+        s"""|{
+            |  "pyStatusValue": "Success"
+            |}
+            |""".stripMargin
+      ).as("application/json")
+  }
+
 }
